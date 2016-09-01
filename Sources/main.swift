516801117dc8d5d56f973560d7098feae75e0255
Logan Wright <logan.william.wright@gmail.com>
2016-08-31 23:32:14 -0400

    it works sort of

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,18 @@
 import Vapor
+import Leaf
 
 let drop = Droplet()
+
+drop.post("render") { request in
+    guard
+        let json = try request.data["json"]?.string.flatMap({ try JSON(bytes: $0.bytes) }),
+        let rawLeaf = request.data["leaf"]?.string
+        else { return "Oh no!" }
+
+    let stem = Stem(workingDirectory: "./")
+    let leaf = try stem.spawnLeaf(raw: rawLeaf)
+    let rendered = try stem.render(leaf, with: Context(json.makeNode()))
+    return rendered.string
+}
+
 drop.serve()
