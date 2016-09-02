d3cfa6781d5927a69ca7587e141615d6cbd3a1b9
Logan Wright <logan.william.wright@gmail.com>
2016-09-02 13:35:42 -0400

    update highlighting

--- Public/ace-builds/src-min/mode-leaf.js
+++ Public/ace-builds/src-min/mode-leaf.js
@@ -6,51 +6,27 @@ ace.define("ace/mode/leaf_highlight_rules",["require","exports","module","ace/li
 
            var LeafHighlightRules = function() {
 
-           var keywords = (
-                           "select|insert|update|delete|from|where|and|or|group|by|order|limit|offset|having|as|case|" +
-                           "when|else|end|type|left|right|join|on|outer|desc|asc|union|create|table|primary|key|if|" +
-                           "foreign|not|references|default|null|inner|cross|natural|database|drop|grant"
-                           );
-
-           var builtinConstants = (
-                                   "true|false"
-                                   );
-
-           var builtinFunctions = (
-                                   "avg|count|first|last|max|min|sum|ucase|lcase|mid|len|round|rank|now|format|" +
-                                   "coalesce|ifnull|isnull|nvl"
-                                   );
-
-           var dataTypes = (
-                            "int|numeric|decimal|date|varchar|char|bigint|float|double|bit|binary|text|set|timestamp|" +
-                            "money|real|number|integer"
-                            );
-
-           var keywordMapper = this.createKeywordMapper({
-                                                        "support.function": builtinFunctions,
-                                                        "keyword": keywords,
-                                                        "constant.language": builtinConstants,
-                                                        "storage.type": dataTypes
-                                                        }, "identifier", true);
-
            this.$rules = {
            "start" : [
                       {
-                      token : "keyword.operator",
+                      token : "constant.language",
                       regex : "\\#",
                       next: "tag"
                       }
                       ],
            "tag" : [
                     {
-                    token : "keyword.operator",
+                    token : "constant.language",
+                    regex : "\\#",
+                    next: "tag"
+                    },
+                    {
+                    token : "constant.language",
                     regex : "\\(",
                     next : "parameter-list"
-
                     },
                     {
                     defaultToken : "keyword"
-
                     }
                     ],
            "parameter-list" : [
@@ -63,45 +39,44 @@ ace.define("ace/mode/leaf_highlight_rules",["require","exports","module","ace/li
                                regex : "\\,"
                                },
                                {
-                               token : "keyword.operator",
+                               token : "constant.language",
                                regex : "\\)",
                                next : "start"
                                },
                                {
-                               defaultToken : "keyword.variable"
-
+                               defaultToken : "variable.constant"
                                }
                                ]
            };
 
            this.normalizeRules();
            };
-           
+
            oop.inherits(LeafHighlightRules, TextHighlightRules);
-           
+
            exports.LeafHighlightRules = LeafHighlightRules;
            });
 
 ace.define("ace/mode/leaf",["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/leaf_highlight_rules","ace/range"], function(require, exports, module) {
            "use strict";
-           
+
            var oop = require("../lib/oop");
            var TextMode = require("./text").Mode;
            var LeafHighlightRules = require("./leaf_highlight_rules").LeafHighlightRules;
            var Range = require("../range").Range;
-           
+
            var Mode = function() {
            this.HighlightRules = LeafHighlightRules;
            };
            oop.inherits(Mode, TextMode);
-           
+
            (function() {
-            
+
             this.lineCommentStart = "--";
-            
+
             this.$id = "ace/mode/leaf";
             }).call(Mode.prototype);
-           
+
            exports.Mode = Mode;
-           
+
            });
